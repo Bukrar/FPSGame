492383e994d1194a1223807b1f609f0a0e57a51f
Bukrar <alex968624229@gmail.com>
2019-03-09 01:45:50 +0800

    腳色移動程式

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 6f;
+    private Vector3 movement;
+    private Animator animator;
+    private Rigidbody playerRigidbody;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Move(float h ,float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    private void FixedUpdate()
+    {    
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Move(h,v);
+    }
+}
